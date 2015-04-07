2556e331449a84b9b5b3643d5e2e214cd6c5151e
tripack <tripack@126.com>
2015-04-07 19:58:42 +0800

    Revert "Save Setting"
    This reverts commit ae68811e67fe1e1dec96271000e96a361b0dbc50.

--- LiMing2015PCClient/Main.vb
+++ LiMing2015PCClient/Main.vb
@@ -11,7 +11,6 @@ Public Class Form_ORRM
         logfile.Close()
         Thread_Connection.Abort()
         Thread_GamePad.Abort()
-        TextBox_Console_Log.Text = My.Settings.Last_Cmd_Line
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -38,8 +37,6 @@ Public Class Form_ORRM
 
     Private Sub Button_ConsoleSend_Click(sender As Object, e As EventArgs) Handles Button_ConsoleSend.Click
         If TextBox_ConsoleSend.Text <> "" Then
-            My.Settings.Last_Cmd_Line = TextBox_ConsoleSend.Text
-            My.Settings.Save()
             If Global_Var.Com_TextMode Then
                 Out_Buffer.Send_Text(TextBox_ConsoleSend.Text)
                 TextBox_ConsoleSend.Text = ""
