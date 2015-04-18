a60c3c7cd934a22bd61e6a464b4241bb239eefbe
Kinghsy <136448624@qq.com>
2015-04-18 21:02:46 +0800

    操作号的确定

--- LiMing2015PCClient/Global_Var.vb
+++ LiMing2015PCClient/Global_Var.vb
@@ -53,14 +53,13 @@
         Abort = &HFF  'No Argument
         ConfirmCMD = &HF0 'Arg1:int,instruction No. Arg2. Empty
         Set_DMotor = &HA0 ' Arg1:int,spdL;Arg2:int,spdR
+        Set_DHolder = &HA1 'Arg1:int x, Arg2:empty
         HandShake = &HF1 'No Arg.
         HandConfirm = &HF2 'No Arg.
         GamePad_X_Down = &HB0 'No Argument
         GamePad_Y_Down = &HB1
-        'GamePad_A_Down = &HB2
-        'GamePad_B_Down = &HB3
-        GamePad_A_Down = &H1
-        GamePad_B_Down = &H2
+        GamePad_A_Down = &HB2
+        GamePad_B_Down = &HB3
         GamePad_X_Up = &HB4 'No Argument
         GamePad_Y_Up = &HB5
         GamePad_A_Up = &HB6
@@ -73,8 +72,14 @@
         GamePad_R_Up = &HC5
         GamePad_U_Up = &HC6
         GamePad_D_Up = &HC7
-        GamePad_RB_DOWN = &HD0
+        GamePad_RB_Down = &HD0
         GamePad_RB_Up = &HD1
+        GamePad_LB_Down = &HD2
+        GamePad_LB_Up = &HD3
+        GamePad_LT_Down = &HD5
+        GamePad_LT_Up = &HD6
+        GamePad_RT_Down = &HD7
+        GamePad_RT_Up = &HD8
 
     End Enum
 
